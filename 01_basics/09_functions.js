d3e5bb4a404e195ce9196e6c59da29022cfcb020
bq-shruti-59 <shruti.shrivastava@betaque.com>
2025-02-24 20:07:13 +0530

    Update 09_functions.js

--- 01_basics/09_functions.js
+++ 01_basics/09_functions.js
@@ -174,19 +174,538 @@ const abc =function fun2(){
 // work: to execute immediately after being initialized
 // need: to avoid any changes through global scope variables/ global pollution
 
-;(function (){
-    console.log(`DB Connected`);
+// ;(function (){
+//     console.log(`DB Connected`);
+// })();
+
+// // ; is necessary for it to know where the function starts and ends
+
+// (() => {
+//     console.log(`DB Connected neww`);
+// })();
+
+
+// // passing parameter in IIFE
+
+// ((name) => {
+//     console.log(`DB Connected neww ${name}`);
+// })("shruti");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------functions again----------------------------
+
+// //ANNONYMOUS FUNCTION
+// function (name){                // gives syntax error
+//     return `Hey this is ${name}`
+// }
+
+// // Anonymous functions are only valid in some use cases, 
+// // such as using them in callbacks as well as in an IIFE
+
+
+
+// FUNCTION EXPRESSIONS
+// const myname = function (name){                // gives syntax error
+//     return `Hey this is ${name}`
+// }
+
+// console.log(myname("shruti"))
+
+
+
+
+// //--------- hoisting --------------------
+// // Functions that are declared with the function keyword are called hoisted.
+// // JavaScript does not hoist variable functions.
+
+// console.log(display("shruti"))
+// console.log(displayvar("shruti"))   // cannot access before initialization
+
+// function display (username) {
+//     return `hey this is ${username}`
+// }
+
+// const displayvar = function (username){
+//     return `Hey this is ${username}`
+// }
+
+
+
+
+
+// // ARROW FUNCTIONS
+// // are also anonymous functions
+// // You always have to stick it into a variable
+// // allow for writing one line functions
+// // do not have their own scope in reference to the this keyword 
+
+
+// const inchToCm = (inches) => {
+//     return inches * 2.54;
+// }
+
+// console.log(inchToCm(3));
+
+
+// // EXPLICIT RETURN 
+// // An explicit return is when you type the return keyword before returning a value
+// // exactly what we did above
+
+
+// // IMPLICIT RETURN
+// // returning it without actually having to type the keyword return
+
+
+// const inchToCm2 = (inches) => (inches * 2.54);
+// console.log(inchToCm2(3));
+
+
+// const add = (a, b=3) => (a + b)
+// console.log(add(3, 5))
+
+
+
+
+// // returning an object 
+
+// function makePerson(first, last) {
+//     const person = {
+//         name: `${first} ${last}`,
+//         age: 2
+//     };
+//     return person;
+// }
+
+
+// // converting this into an arrow function
+// const makePerson2 = (first, last) => ({
+//         name: `${first} ${last}`,
+//         age: 2
+//     }
+// )
+// console.log(makePerson2("shruti", "shri"))
+
+
+
+
+// // IIFE
+// // immediately invoked function expression
+
+// // function (){
+// //     console.log('running an anno function');
+// //     return 'you are cool';
+// // }
+
+// // one way to run this function is using a variable
+// // anothor way is to use iife
+
+// // ;(function(){
+// //     console.log('running an anno function');
+// //     return 'you are cool'
+// // })();
+
+
+// // with parameter
+// ;(function(age){
+//     console.log('running an anno function');
+//     console.log( `you are cool ${age}`)
+// })(455);
+
+
+// example
+// without iife (global pollution)
+let counter =0;
+
+function inc (){
+    counter++;
+}
+
+inc();
+console.log(counter)
+// Here, counter is in the global scope and can be accessed or modified anywhere, which may cause unexpected behavior.
+
+
+
+// using iife
+;(function inc(){
+    let counter = 0;    //private variable
+
+    function inc(){
+        counter++;
+    }
+    inc();
+    inc();
+
 })();
-
-// ; is necessary for it to know where the function starts and ends
-
-(() => {
-    console.log(`DB Connected neww`);
-})();
+console.log(counter);   // undefined
 
 
-// passing parameter in IIFE
 
-((name) => {
-    console.log(`DB Connected neww ${name}`);
-})("shruti");
+
+
+// METHODS
+
+// A method is simply a function that lives inside of an object.
+// in console.log(),
+// console is an object 
+// log() is a method
+
+const wes = {
+    name: "shruti",
+    sayHi: function (){
+        console.log(this);
+        console.log("hey shruti");
+        return "hey shruti";
+    },
+    yellHi() {
+        console.log("HEY WESSSSS");
+    },
+    whisperHi: () => {
+        console.log('hiii wess im a mouse');
+    }
+};
+// console.log(wes.sayHi())
+// console.log(wes.yellHi())
+// console.log(wes.whisperHi())
+
+
+
+
+
+// const wes = {
+//     name: "shruti",
+//     sayHi: function sayHi(){
+//         console.log(`hey ${this.name}`);
+//         return `hey shruti`;
+//     }
+// }
+// console.log(wes.sayHi())
+// // That will not work in an arrow function because they take the parent scope of this
+
+
+
+
+// CALLBACK FUNCTIONS
+// callback function is a function that gets passed into another function and then it is called by the browser at a later point in time.
+// means either when someone clicks something, run this. Or when this amount of time has passed, run this.
+// types: click callback, timer callback
+
+// // click callback
+// const button = document.querySelector(".clickMe");
+// console.log(button)
+
+// button.addEventListener("click", wes.sayHi);
+
+
+// //another example
+// function handleClick(){
+//     console.log("great clicking");
+// }
+// button.addEventListener("click", handleClick);
+
+
+
+// // timer callback
+
+// // setTimeout();
+// // It takes two things:
+
+// // a function to call after a certain amount of time
+// // a duration in milliseconds (after how long should I run this)
+
+// setTimeout(wes.sayHi, 2000)
+
+
+
+// // You can also pass it an anonymous function.
+
+// setTimeout(function () {
+//     console.log("we are trying timer callback")
+// }, 2000)
+
+
+// // You can pass those as arrow functions as well.
+
+// setTimeout(() => {
+//     console.log("did this using settimeout in arrow function")
+// }, 4000)
+
+
+// myVar = setTimeout(function, milliseconds);
+// clearTimeout(myVar);
+
+// If the function has not already been executed, 
+// you can stop the execution by calling the clearTimeout() method
+
+
+
+
+
+// // set interval
+// setInterval(function, milliseconds);
+// The first parameter is the function to be executed.
+
+// The second parameter indicates the length of the time-interval between each execution.
+
+let myInterval = setInterval(wes.sayHi, 1000)
+
+clearInterval(myInterval);
+
+
+
+
+
+// WAYS TO CALL A FUNCTION
+// - call as a function
+// - call as a method
+// - call a function with the constructor
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(5, 6))
+
+// This is a common way to invoke a JavaScript function, but not a very good practice.
+// Global variables, methods, or functions can easily create name conflicts and bugs in the global object.
+
+
+
+
+
+// caling as a method
+const myObject = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+console.log(myObject.fullName());  
+// The thing called this, is the object that "owns" the JavaScript code. In this case the value of this is myObject.
+
+
+// calling function as constructor
+
+function myFunction(arg1, arg2) {
+    this.firstName = arg1;
+    this.lastName = arg2
+}
+
+const myObj = new myFunction("shruti", "shrivastava")
+
+console.log(myObj.firstName)
+
+
+
+
+
+
+// CALL BIND APPLY, THIS
+
+// this
+
+// this keyword refers to an object.
+
+// In an object method, this refers to the object.
+// Alone, this refers to the global object.
+// In a function, this refers to the global object.
+// In a function, in strict mode, this is undefined.
+// In an event, this refers to the element that received the event.
+// Methods like call(), apply(), and bind() can refer this to any object.
+
+
+// const person = {
+//     firstName: "shruti",
+//     lastName: "shri", 
+//     fullName: function (){
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+// console.log(person.fullName());
+
+// In the example above, this refers to the person object.
+
+// this is not a variable. It is a keyword. You cannot change the value of this.
+
+
+
+
+// call() method
+
+// The call() method is a predefined JavaScript method.
+
+// It can be used to invoke (call) a method with an object as an argument (parameter).
+
+// With call(), an object can use a method belonging to another object.
+
+
+// const person = {
+//     fullName: function () {
+//         return this.firstName+ " " + this.lastName;
+//     }
+// }
+
+// person1 = {
+//     firstName: "shruti",
+//     lastName: "shri"
+// }
+
+// person2 = {
+//     firstName: "alex",
+//     lastName: "alice"
+// }
+// console.log(person.fullName.call(person1))
+
+
+
+
+// // call method with arguments
+// const person = {
+//     fullName: function (city, country) {
+//         return `${this.firstName} ${this.lastName} ${city} ${country}`;
+//     }
+// }
+
+// person1 = {
+//     firstName: "shruti",
+//     lastName: "shri"
+// }
+
+// person2 = {
+//     firstName: "alex",
+//     lastName: "alice"
+// }
+// console.log(person.fullName.call(person1, "bhopal", "ind"))
+
+
+
+
+
+
+// apply() method
+
+// With the apply() method, you can write a method that can be used on different objects.
+
+// const person = {
+//     fullName: function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   }
+  
+//   const person1 = {
+//     firstName: "Mary",
+//     lastName: "Doe"
+//   }
+  
+//   // This will return "Mary Doe":
+//   person.fullName.apply(person1);
+
+
+
+// The Difference Between call() and apply()
+// The difference is:
+
+// The call() method takes arguments separately.
+
+// The apply() method takes arguments as an array.
+
+
+// const person = {
+//     fullName: function(city, country) {
+//       return this.firstName + " " + this.lastName + "," + city + "," + country;
+//     }
+//   }
+  
+//   const person1 = {
+//     firstName:"John",
+//     lastName: "Doe"
+//   }
+  
+//   person.fullName.apply(person1, ["Oslo", "Norway"])
+
+
+
+// JavaScript Strict Mode
+// In JavaScript strict mode, if the first argument of the apply() method is not an object, 
+// it becomes the owner (object) of the invoked function. In "non-strict" mode, it becomes the global object.
+
+
+
+
+// bind()
+
+// With the bind() method, an object can borrow a method from another object.
+
+// const person = {
+//     firstName:"John",
+//     lastName: "Doe",
+//     fullName: function () {
+//       return this.firstName + " " + this.lastName;
+//     }
+// }
+  
+// const member = {
+//     firstName:"Hege",
+//     lastName: "Nilsen",
+// }
+  
+// let fullName = person.fullName.bind(member);
+
+// console.log(fullName)
+
+
+// preserving this 
+
+// const person = {
+//     firstName: "shruti",
+//     lastName: "shri",
+//     display: function() {
+//         let button = document.querySelector(".clickMe");
+//         button.innerHTML = `${this.firstName} ${this.lastName}`;
+//     }
+// }
+// setTimeout(person.display, 3000);
+  
+
+// When a function is used as a callback, this is lost.
+// This example will try to display the person name after 3 seconds, but it will display undefined instead
+// we tried to change the button content
+
+
+
+// the bind() method is used to bind person.display to person
+
+// const person = {
+//     firstName: "shruti",
+//     lastName: "shri",
+//     display: function() {
+//         let button = document.querySelector(".clickMe");
+//         button.innerHTML = `${this.firstName} ${this.lastName}`;
+//     }
+// }
+// let display = person.display.bind(person);
+// setTimeout(display, 3000);
+// // this works
+
+
+
+
+
+
+
